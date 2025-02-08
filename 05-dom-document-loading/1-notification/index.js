0d9f43569691ab64caecdfcb1d77a01cd7311742
VK <belsynth@gmail.com>
2025-02-08 17:59:23 +0300

    Добавляет реализацию класса NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  static lastShownNotification;
 
+  constructor(message = '', {
+    duration = 2000,
+    type = 'success'
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.durationInSeconds = duration / 1000;
+    this.type = type;
+
+    this.element = this.createElement();
+  }
+
+  createTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationInSeconds}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type[0].toUpperCase() + this.type.slice(1)} notification</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  createElement() {
+    const elem = document.createElement('div');
+    elem.innerHTML = this.createTemplate();
+
+    return elem.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.lastShownNotification) {
+      NotificationMessage.lastShownNotification.remove();
+    }
+
+    NotificationMessage.lastShownNotification = this;
+
+    target.append(this.element);
+
+    this.hide();
+  }
+
+  hide() {
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
